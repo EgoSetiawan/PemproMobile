495df6d870354eee8c669630772b41f739a50243
EgoSetiawan <muhammadteguh1126@gmail.com>
2023-06-11 22:30:03 +0700

    add regis

--- lib/view/component/app_bar.dart
+++ lib/view/component/app_bar.dart
@@ -1,14 +1,35 @@
 import 'package:flutter/material.dart';
+// ignore: unused_import
 import 'package:flutter_svg/flutter_svg.dart';
 
-class AppBarWidget extends StatelessWidget {
-  const AppBarWidget({super.key});
+class AppBarWidgets extends StatelessWidget implements PreferredSize {
+  const AppBarWidgets({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-      title: SvgPicture.asset('../assests/icon_touring_jember.svg'),
+      // title: Text('aww'),
+      // title: SvgPicture.asset('assets/icon_touring_jember.svg'),
+      actions: [
+        SizedBox(
+          height: 18.0,
+          width: 18.0,
+          child: IconButton(
+            icon: SvgPicture.asset('assets/icon_touring_jember.svg'),
+            onPressed: () => {},
+          ),
+        )
+      ],
+      // leading: SvgPicture.asset(''),
     ));
   }
+
+  @override
+  // TODO: implement child
+  Widget get child => throw UnimplementedError();
+
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
