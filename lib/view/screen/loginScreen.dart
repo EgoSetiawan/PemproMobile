f1e96a41986cd84aa25986fbf2355af967eabe93
Yoga Firdaus Pratikha <62527088+Yusakha@users.noreply.github.com>
2023-06-18 19:12:35 +0700

    Update AppBar, UI, and Costum Fonts

--- lib/view/screen/loginScreen.dart
+++ lib/view/screen/loginScreen.dart
@@ -30,10 +30,11 @@ class _LoginScreenState extends State<LoginScreen> {
   Future<LoginResponse>? _loginFuture;
   @override
   Widget build(BuildContext context) {
+    const Scaffold(appBar: AppBarWidgets());
     return Container(
         decoration: const BoxDecoration(
             image: DecorationImage(
-                image: AssetImage('assests/diatas_gunung.png'),
+                image: AssetImage('assests/images/gunung_biru.png'),
                 fit: BoxFit.cover)),
         child: SizedBox(
           child: Align(
@@ -44,44 +45,49 @@ class _LoginScreenState extends State<LoginScreen> {
               child: Card(
                   color: Colors.white,
                   child: Padding(
-                    padding: EdgeInsets.all(20),
+                    padding: EdgeInsets.only(
+                        top: 24, left: 18, right: 18, bottom: 24),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        const Text(
+                        Text(
                           "Masuk",
                           style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                            fontSize: 24,
                           ),
                         ),
-                        const Text("Email"),
+                        Container(
+                            margin: EdgeInsets.only(top: 20),
+                            child: Text(
+                              "Email",
+                              textAlign: TextAlign.justify,
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w400, fontSize: 16),
+                            )),
                         Container(
                           decoration: BoxDecoration(
-                              border: Border.all(
-                                  color: Colors.black,
-                                  width: 1
-                              ),
-                              borderRadius: BorderRadius.circular(8)
-                          ),
+                              border: Border.all(color: Colors.black, width: 1),
+                              borderRadius: BorderRadius.circular(8)),
                           child: Padding(
-                            padding: EdgeInsets.only(left:8),
+                            padding: EdgeInsets.only(left: 8),
                             child: TextField(
                               controller: _email,
                             ),
                           ),
                         ),
-                        const Text("Password"),
+                        const Text(
+                          "Password",
+                          textAlign: TextAlign.justify,
+                          style: TextStyle(
+                              fontWeight: FontWeight.w400, fontSize: 16),
+                        ),
                         Container(
                           decoration: BoxDecoration(
-                              border: Border.all(
-                                  color: Colors.black,
-                                  width: 1
-                              ),
-                              borderRadius: BorderRadius.circular(8)
-                          ),
+                              border: Border.all(color: Colors.black, width: 1),
+                              borderRadius: BorderRadius.circular(8)),
                           child: Padding(
-                            padding: EdgeInsets.only(left:8),
+                            padding: EdgeInsets.only(left: 8),
                             child: TextField(
                               controller: _password,
                             ),
@@ -91,23 +97,27 @@ class _LoginScreenState extends State<LoginScreen> {
                           margin: EdgeInsets.only(top: 20),
                           child: ElevatedButton(
                             onPressed: () {
-                              setState(() {
-                                _loginFuture = doLogin(_email.text, _password.text);
-                              });
+                              // setState(() {
+                              //   _loginFuture = doLogin(_email.text, _password.text);
+                              // });
+                              _login();
                             },
                             style: ElevatedButton.styleFrom(
                               primary: greenColor,
                               onPrimary: Colors.white,
                             ),
                             child: const SizedBox(
-                                width: double.infinity, // Mengatur lebar Container agar mengisi seluruh parent
+                                width: double
+                                    .infinity, // Mengatur lebar Container agar mengisi seluruh parent
                                 child: Center(
-                                  child: Text("Login"),
-                                )
-                            ),
+                                  child: Text("Login",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w500,
+                                        // fontSize: 24,
+                                      )),
+                                )),
                           ),
                         ),
-
                         Container(
                           margin: EdgeInsets.only(top: 20),
                           child: ElevatedButton(
@@ -115,21 +125,23 @@ class _LoginScreenState extends State<LoginScreen> {
                               Navigator.push(
                                   context,
                                   MaterialPageRoute(
-                                      builder: (context) =>
-                                          RegisterScreen()));
+                                      builder: (context) => RegisterScreen()));
                             },
                             style: ElevatedButton.styleFrom(
-                              primary: greenColor,
-                              onPrimary: Colors.white,
+                              primary: Colors.white,
+                              onPrimary: Colors.black,
                             ),
                             child: Container(
-                                width: double.infinity, // Mengatur lebar Container agar mengisi seluruh parent
+                                width: double
+                                    .infinity, // Mengatur lebar Container agar mengisi seluruh parent
                                 child: Center(
-                                  child: Text("Daftar"),
-                                )
-                            ),
+                                  child: Text("Belum mempunyai akun? Daftar",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.w200,
+                                          fontSize: 12)),
+                                )),
                           ),
-                        )
+                        ),
                       ],
                     ),
                   )),
@@ -138,25 +150,62 @@ class _LoginScreenState extends State<LoginScreen> {
         ));
   }
 
-  Future<LoginResponse> doLogin(String email , String password) async{
-    try {
-      Map<String,dynamic> requestBody ={
-        'email' : email , 'password' :password
-      };
-      final response = await http.post(Uri.parse("https://732a-125-166-118-148.ngrok-free.app/login"),body: requestBody);
-      if (response.statusCode == 200) {
-        Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>SecondScreen()),);
+  // Future<LoginResponse> doLogin(String email , String password) async{
+  //   try {
+  //     Map<String,dynamic> requestBody ={
+  //       'email' : email , 'password' :password
+  //     };
+  //     final response = await http.post(Uri.parse("https://732a-125-166-118-148.ngrok-free.app/login"),body: requestBody);
+  //     if (response.statusCode == 200) {
+  //       Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>SecondScreen()),);
+  //
+  //       return LoginResponse.fromJson(json.decode(response.body));
+  //     } else {
+  //       throw Exception("something went wrong HEHE");
+  //     }
+  //   } catch (e) {
+  //     Fluttertoast.showToast(
+  //         msg: "something went wrong KOCAK",
+  //         toastLength: Toast.LENGTH_SHORT,
+  //         gravity: ToastGravity.BOTTOM);
+  //     throw Exception("something went wrong KCCAK");
+  //   }
+  // }
 
-        return LoginResponse.fromJson(json.decode(response.body));
-      } else {
-        throw Exception("something went wrong HEHE");
+  Future<LoginResponse> doLoginx(String email, String password) async {
+    try {
+      Map<String, dynamic> requestBody = {'email': email, 'password': password};
+      final response = await http.post(
+          Uri.parse("https://5489-118-99-83-51.ngrok-free.app/auth/login"),
+          body: requestBody);
+      return LoginResponse.fromJson(json.decode(response.body));
+    } catch (e) {
+      throw Exception("Terjadi kesalahan saat Login");
+    }
+  }
+
+  Future<void> _login() async {
+    try {
+      final loginResult = await doLoginx(_email.text, _password.text);
+
+      Fluttertoast.showToast(
+        msg: loginResult.message,
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+      );
+      if (!loginResult.error) {
+        // await UserSimplePreferences.setUsername(loginResult.loginResult.userId);
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => SecondScreen()),
+        );
       }
     } catch (e) {
       Fluttertoast.showToast(
-          msg: "something went wrong KOCAK",
-          toastLength: Toast.LENGTH_SHORT,
-          gravity: ToastGravity.BOTTOM);
-      throw Exception("something went wrong KCCAK");
+        msg: "Terjadi kesalahan saat Login",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+      );
     }
   }
 }
